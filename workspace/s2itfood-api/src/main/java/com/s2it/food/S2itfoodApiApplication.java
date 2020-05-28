8f9d1087f152745ae036e4a4e4dcc5103713f291
slsmoraes <slsmoraes@gmail.com>
2020-05-27 23:40:33 -0300

    Inicio

---
+++ workspace/s2itfood-api/src/main/java/com/s2it/food/S2itfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.s2it.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2itfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2itfoodApiApplication.class, args);
+	}
+
+}
